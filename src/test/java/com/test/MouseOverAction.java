c211801287764ad8845edcdf1a001ab956c9a33d
VenkatWaran1 <venkatwaran94@gmail.com>
2023-03-04 15:10:28 +0530

    My Task

---
+++ src/test/java/com/test/MouseOverAction.java
@@ -0,0 +1,41 @@
+package com.test;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MouseOverAction {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://codoid.com/");
+		
+		WebElement ourProd = driver.findElement(By.xpath("//a[text()='Our Products'][1]"));
+		Actions action = new Actions(driver);
+		action.moveToElement(ourProd).build().perform();
+		
+		Thread.sleep(2000);
+		
+		WebElement visGrid = driver.findElement(By.xpath("//a[text()='VisGrid']"));
+		action.moveToElement(visGrid).click().perform();
+		
+		Thread.sleep(2000);
+		
+		WebElement relNotes = driver.findElement(By.xpath("//a[text()='release notes']"));
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].click()", relNotes);
+		
+		
+	}
+
+}
---
+++ src/test/java/com/test/ScreenshotTask.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScreenshotTask {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://codoid.com/");
+		
+		TakesScreenshot tsc = (TakesScreenshot) driver;
+		File src = tsc.getScreenshotAs(OutputType.FILE);
+		File dest = new File("E:\\Screenshot\\ScreenShot1.jpg");
+		
+		FileUtils.copyFile(src, dest);
+		
+	}
+
+}
